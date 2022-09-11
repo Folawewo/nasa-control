92220f65bfd1ebf0364ec82b72eb8454478e00df
Folawewo Anthony <famwallexie@gmail.com>
2022-09-11 09:46:39 +0100

    API endpoints

--- server/src/routes/planets/planets.router.js
+++ server/src/routes/planets/planets.router.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
-const planetsController = require('./planets.controller');
+const {
+    getAllPlanets,
+} = require('./planets.controller');
 
 const planetsRouter = express.Router();
 
-planetsRouter.get('/planets', planetsController.getAllPlanets);
+planetsRouter.get('/planets', getAllPlanets);
 
 module.exports = planetsRouter;
