84e0c0c3122625af18d88fd3e5c0659931e28623
Folawewo Anthony <famwallexie@gmail.com>
2022-09-19 02:30:08 +0100

    connecting POST launches endpoint

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -7,7 +7,7 @@ const launch = {
   mission: 'Kepler Exploration X',
   rocket: 'Explorer ISI',
   launchDate: new Date('December 27, 2030'),
-  destination: 'Kepler-442 b',
+  target: 'Kepler-442 b',
   customer: ['SPACEX', 'NASA'],
   upcoming: true,
   success: true,
