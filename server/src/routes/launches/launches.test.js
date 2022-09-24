1c52fababb0be31137ea777bb9e31d4301423c02
Folawewo Anthony <famwallexie@gmail.com>
2022-09-24 22:40:53 +0100

    testing api endpoints with supertest

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -11,19 +11,31 @@ describe('Test GET /launches', () => {
 });
 
 describe('Test POST /launch', () => {
+  const completeLaunchDate = {
+    mission: 'USS Enterprise',
+    rocket: 'NCC 170-D',
+    target: 'Kepler-186 f',
+    launchDate: 'January 4, 2028',
+  };
+
+  const launchDataWithoutDate = {
+    mission: 'USS Enterprise',
+    rocket: 'NCC 170-D',
+    target: 'Kepler-186 f',
+  }
+
   test('It should respond with 201 created', async () => {
     const response = await request(app)
       .post('/launches')
-      .send({
-        mission: 'USS Enterprise',
-        rocket: 'NCC 170-D',
-        target: 'Kepler-186 f',
-        launchDate: 'January 4, 2028',
-      })
+      .send()
       .expect('Content-Type', /json/)
       .expect(201);
 
-      expect(response.body)
+    const requestDate = expect(response.body).toMatchObject({
+      mission: 'USS Enterprise',
+      rocket: 'NCC 170-D',
+      target: 'Kepler-186 f',
+    });
   });
   test('It should catch missing required properties', () => {});
   test('It should catch invalid dates', () => {});
