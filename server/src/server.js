80544186eeff0abb909cd2c7d9884548ad0157a8
Folawewo Anthony <famwallexie@gmail.com>
2022-10-01 20:05:32 +0100

    connecting to database

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,23 @@
 const http = require('http');
+const mongoose = require('mongoose');
 const app = require('./app');
 
 const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL =
+  'mongodb+srv://nasa-api:w7gBHa1srPjhPI57@nasacluster.vntv8zj.mongodb.net/nasa?retryWrites=true&w=majority';
+
 const server = http.createServer(app);
 
 async function startServer() {
+  await mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useFindAndModify: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  });
   await loadPlanetsData();
 
   server.listen(PORT, () => {
